ba45d4d96d156f138784e32c5ed3b3bf551556b0
Reginald Diocera <rmdiocera@gmail.com>
2019-06-29 11:51:45 +0800

    Added "Delete" option

--- application/views/post/post.php
+++ application/views/post/post.php
@@ -3,4 +3,6 @@
     <span class="post_date text-center px-2 py-1">Posted on <?= $post['created_at'] ?></span>
     <br><br>
     <p class="my-2"><?= $post['body'] ?></p><br>
+    <?= form_open('nf_post/delete/'.$post['id'])?>
+    <input type="submit" value="Delete" class="btn btn-danger">;
 </div>
