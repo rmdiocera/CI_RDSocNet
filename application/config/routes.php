54a4d9853c9d3da42df8c72aa287e48e70b8d5a0
Reginald Diocera <rmdiocera@gmail.com>
2019-07-04 21:30:15 +0800

    Added routes for user functions and pagination

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,8 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // $route['feed/(:any)'] = 'feed/post/$1';
+$route['feed/(:num)'] = 'feed/index/$1';	
+$route['logout'] = 'users/logout';
+$route['login'] = 'users/login';
+$route['register'] = 'users/register';
 $route['post/(:any)'] = 'nf_post/post/$1';
-$route['default_controller'] = 'feed/index';
-$route['(:any)'] = 'feed/index/$1';
+$route['feed'] = 'feed/index';
+$route['about'] = 'pages/about';
+// $route['default_controller'] = 'feed/index';
+$route['default_controller'] = 'pages/home';
+// $route['(:any)'] = 'feed/index/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
